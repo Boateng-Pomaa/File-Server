72376481275f7b4b28a6e3e9bdab2ec8a67545e0
Boateng Pomaa Linda <lindapomaa27@gmail.com>
2023-04-06 14:08:43 +0100

    Added a middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken"
+import userModel from "../models/userSchema"
+
+export const protect = async(req,res,next) =>{
+
+    let token
+    //get token from header
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer'))
+        try {
+            //getting tken from header
+            token = req.headers.authorization.split(' ')[1]
+
+            //verifying the token
+            const decoded = jwt.verify(token,process.env.JWT_SECRET)
+
+            //assigning user to request object
+            req.user = await userModel.findById(decoded.id).select('-password')
+
+            next()
+        } catch (error) {
+            res.status(401).json({
+                message:"Token is not valid"
+            })
+        }
+        if(!token){
+            res.status(401).json({
+                message:"Authorization Denied, No token"
+            })
+        }
+    }
