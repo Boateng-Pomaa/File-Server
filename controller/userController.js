ad1fc9f358307ef4ad1767b06402d7a42a75f4c1
Boateng Pomaa Linda <lindapomaa27@gmail.com>
2023-04-08 16:23:46 +0100

    added user controller

--- controller/userController.js
+++ controller/userController.js
@@ -1,6 +1,97 @@
-import userModel from '../models/userSchema'
+import {userModel} from '../models/userSchema'
 import jwt from "jsonwebtoken"
+import bcrypt from 'bcrypt'
 
 
+
+export async function registerUser(req,res){
+
+    try{
+        const {email, password, userRole} = req.body;
+
+    // Validation
+  if (!email || !password || !userRole) {
+    res.status(400).json({
+        message:'Please include all fields'})
+  }
+
+  // Find if user already exists
+  const userExists = await userModel.findOne({ email })
+
+  if (userExists) {
+    res.status(400).json({
+        message:'User already exists'})
+  }
+
+  // Hash Password
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+  // CREATING USER
+    const user = await userModel.create({
+        username,
+        email,
+        password:hashedPassword,
+        userRole:userRole || "user"
+    })
+
+    /// function to generate accesstoken
+  const token = jwt.sign({user_id:user._id,email:user.email},process.env.SECRET, {
+      expiresIn: "1d"
+     });
+
+     user.accessToken = token
+
+    if (user){
+        res.status(200).json({
+            message:'Registration Successful',
+            user
+        })
+    }else{
+        res.status(400).json({
+            message:"Registration unsuccessful"
+        })
+    }
+    }catch(err){
+        console.log(err)
+    }
+    
+}
+
+
+
+///LOGGING IN A USER
+
+export async function loginUser(req,res){
+    
+    try {
+        const {email, password} = req.body
+        const user = await userModel.findOne({email})
+        if(user && (await bcrypt.compare(password, user.password)))
+           {
+
+            const token = jwt.sign({user_id:user._id,email:user.email},process.env.SECRET, {
+                expiresIn: "5d"
+               });
+          
+               user.accessToken = token
+
+                res.status(401).json({
+                    message:"Logged in successful",
+                    user
+                    
+                })
+            }else{
+                res.status(400).json({
+                    message:'Invalid Credentials'
+                })
+                   }
+        }
+     catch (err) {
+        console.error(err.message)
+        res.status(500).json({
+            message:'server error'
+        })
+    }
+}
 
 
